fdfba524b6fbe6776841a852f096038fb25c7d1a
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-06-18 22:56:55 +0900

    #2 create usecase for github user

---
+++ app/src/main/java/com/evergreen/todaycommit/domain/usecase/GetGithubUserUseCase.kt
@@ -0,0 +1,13 @@
+package com.evergreen.todaycommit.domain.usecase
+
+import com.evergreen.todaycommit.domain.repository.GithubRepository
+import javax.inject.Inject
+
+class GetGithubUserUseCase @Inject constructor(
+    private val githubUserRepository: GithubRepository
+) {
+    suspend operator fun invoke() = runCatching {
+        githubUserRepository.getUsers()
+    }
+
+}
