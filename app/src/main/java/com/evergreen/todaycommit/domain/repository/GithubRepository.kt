7da0f43d8f1c125c6980487d2654b3b1ca13952e
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-06-18 22:56:42 +0900

    #2 create github user repository

---
+++ app/src/main/java/com/evergreen/todaycommit/domain/repository/GithubRepository.kt
@@ -0,0 +1,7 @@
+package com.evergreen.todaycommit.domain.repository
+
+import com.evergreen.todaycommit.domain.model.GithubUser
+
+interface GithubRepository {
+    suspend fun getUsers(): GithubUser
+}
