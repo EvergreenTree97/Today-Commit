4bf8c15d8f3a887b4ac7dcc9b04fb21a606ba704
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-06-18 22:56:04 +0900

    #2 Change the github api response format

--- app/src/main/java/com/evergreen/todaycommit/data/remote/api/GithubApi.kt
+++ app/src/main/java/com/evergreen/todaycommit/data/remote/api/GithubApi.kt
@@ -1,9 +1,9 @@
 package com.evergreen.todaycommit.data.remote.api
 
-import com.evergreen.todaycommit.data.model.UserResponse
+import com.evergreen.todaycommit.data.model.GithubUserData
 import retrofit2.http.GET
 
 interface GithubApi {
     @GET("/user")
-    fun getUser(): UserResponse
+    fun getUser(): GithubUserData
 }
