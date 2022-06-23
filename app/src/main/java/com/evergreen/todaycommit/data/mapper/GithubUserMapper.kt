f69fbca65603b45112f64a0633967bb418bb8a43
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-06-24 00:05:00 +0900

    #3 : remove toData mapper

--- app/src/main/java/com/evergreen/todaycommit/data/mapper/GithubUserMapper.kt
+++ app/src/main/java/com/evergreen/todaycommit/data/mapper/GithubUserMapper.kt
@@ -3,10 +3,6 @@ package com.evergreen.todaycommit.data.mapper
 import com.evergreen.todaycommit.data.model.GithubUserData
 import com.evergreen.todaycommit.domain.model.GithubUser
 
-internal fun GithubUser.toData() = GithubUserData(
-    followers = followers
-)
-
 internal fun GithubUserData.toDomain() = GithubUser(
     followers = followers
 )
