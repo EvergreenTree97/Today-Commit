df6608d724da8e6eb34b9fe2a7020da19a454f71
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-06-24 00:06:17 +0900

    #3 : add dummy data object

--- app/src/test/java/com/evergreen/todaycommit/data/GithubApiResponseDummy.kt
+++ app/src/test/java/com/evergreen/todaycommit/data/GithubApiResponseDummy.kt
@@ -1,4 +1,7 @@
 package com.evergreen.todaycommit.data
 
+import com.evergreen.todaycommit.util.Utils
+
 object GithubApiResponseDummy {
+    val users = Utils.readResourceString("githubapi/users-response.json")
 }
