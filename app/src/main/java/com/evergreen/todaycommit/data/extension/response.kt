228413ecf74af311076a97c3002def7cbb1a57e3
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-06-24 00:03:31 +0900

    #3 : add retrofit response extension

--- app/src/main/java/com/evergreen/todaycommit/data/extension/response.kt
+++ app/src/main/java/com/evergreen/todaycommit/data/extension/response.kt
@@ -1,2 +1,17 @@
 package com.evergreen.todaycommit.data.extension
 
+import retrofit2.Response
+
+internal fun <T> Response<T>.apiCall(): T {
+    val body = body()
+    return if (isSuccessful && body != null) {
+        body
+    } else {
+        throw IllegalStateException(
+            """
+            Request is fail.
+            Http message: ${errorBody()?.use { it.string() }}
+            """.trimIndent()
+        )
+    }
+}
