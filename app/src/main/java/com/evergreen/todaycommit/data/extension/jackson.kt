d53b6f27e9b9cbf74047043fb79860a17b6db648
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-06-24 00:02:36 +0900

    #3 : jackson Converter extension

--- app/src/main/java/com/evergreen/todaycommit/data/extension/jackson.kt
+++ app/src/main/java/com/evergreen/todaycommit/data/extension/jackson.kt
@@ -1,2 +1,12 @@
 package com.evergreen.todaycommit.data.extension
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+
+@PublishedApi
+internal val JacksonObjectMapper = ObjectMapper()
+    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    .registerKotlinModule()
+
+internal inline fun <reified T> String.toModel(): T = JacksonObjectMapper.readValue(this,T::class.java)
