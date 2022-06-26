9b269de9bb61f0221ac1f4b6dcaccd548da03c9b
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-06-26 23:45:55 +0900

    #4 : change package

--- app/src/main/java/com/evergreen/todaycommit/ui/theme/Color.kt
+++ app/src/main/java/com/evergreen/todaycommit/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.evergreen.todaycommit
+package com.evergreen.todaycommit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/evergreen/todaycommit/ui/theme/Type.kt
+++ app/src/main/java/com/evergreen/todaycommit/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.evergreen.todaycommit
+package com.evergreen.todaycommit.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
