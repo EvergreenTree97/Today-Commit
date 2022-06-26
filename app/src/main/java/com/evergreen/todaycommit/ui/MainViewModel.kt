811085809905c7d833733408b925e8d1f214c584
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-06-26 23:41:12 +0900

    #4 : add MainViewModel

---
+++ app/src/main/java/com/evergreen/todaycommit/ui/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.evergreen.todaycommit.ui
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.evergreen.todaycommit.domain.usecase.GetGithubUserUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val githubUserUseCase: GetGithubUserUseCase
+) : ViewModel() {
+
+    fun fetchUser(){
+        viewModelScope.launch(Dispatchers.IO) {
+            githubUserUseCase("EvergreenTree97").onSuccess {
+                Log.d("성공", "fetchUser: ${it.userName}")
+            }.onFailure {
+                Log.d("실패", "fetchUser: ${it.localizedMessage} ")
+            }
+        }
+
+    }
+}
