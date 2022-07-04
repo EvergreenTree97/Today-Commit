4c294eafc71f30e69e5abc5ea579f68f4aa7e9ef
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-05 01:41:19 +0900

    #5 : detach reuse composable

--- app/src/main/java/com/evergreen/todaycommit/presentation/main/components/MainComposable.kt
+++ app/src/main/java/com/evergreen/todaycommit/presentation/main/components/MainComposable.kt
@@ -1,2 +1,27 @@
 package com.evergreen.todaycommit.presentation.main.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.evergreen.todaycommit.presentation.theme.White
+
+@Composable
+internal fun RoundCornerBox(
+    modifier: Modifier,
+    alignment: Alignment = Alignment.CenterStart,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(20.dp))
+            .background(White),
+        contentAlignment = alignment
+    ) {
+        content()
+    }
+}
