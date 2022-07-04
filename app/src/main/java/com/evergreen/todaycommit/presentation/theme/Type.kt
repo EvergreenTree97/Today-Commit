8c05821e882816609c875f43242159d0acc676c6
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-05 01:40:10 +0900

    #5 : add suit fontfamilly

--- app/src/main/java/com/evergreen/todaycommit/presentation/theme/Type.kt
+++ app/src/main/java/com/evergreen/todaycommit/presentation/theme/Type.kt
@@ -2,9 +2,12 @@ package com.evergreen.todaycommit.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.evergreen.todaycommit.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +18,10 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+val suit = FontFamily(
+    Font(R.font.suit_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.suit_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.suit_semibold, FontWeight.SemiBold, FontStyle.Normal)
 )
