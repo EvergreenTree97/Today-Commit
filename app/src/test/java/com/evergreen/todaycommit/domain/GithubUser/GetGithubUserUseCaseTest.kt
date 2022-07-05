28c86ab204763416c1234c0f297250dc8f0051c6
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-06 00:22:53 +0900

    #7 : usecase test code

--- app/src/test/java/com/evergreen/todaycommit/domain/GithubUser/GetGithubUserUseCaseTest.kt
+++ app/src/test/java/com/evergreen/todaycommit/domain/GithubUser/GetGithubUserUseCaseTest.kt
@@ -1,13 +1,49 @@
 package com.evergreen.todaycommit.domain.GithubUser
 
+
+import com.evergreen.todaycommit.domain.model.GithubUser
+import com.evergreen.todaycommit.domain.usecase.GetGithubUserUseCase
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(MockKExtension::class)
 class GetGithubUserUseCaseTest {
 
-    @Test
-    fun `given when then`(){
+    @MockK
+    private lateinit var githubUserUseCase: GetGithubUserUseCase
 
+
+    @BeforeEach
+    fun setup() {
+        coEvery {
+            githubUserUseCase("name")
+        } returns Result.success(
+            GithubUser(
+                "name", 2022,
+                1, 2, 3, 4, 5, 6, emptyList()
+            )
+        )
+
+    }
+
+    @DisplayName("github use case test success")
+    @Test
+    fun `fetch github contribution is success`() = runTest {
+        val result = githubUserUseCase("name")
+        assert(result.isSuccess)
+    }
+
+    @DisplayName("github use case test failed")
+    @Test
+    fun `fetch github contribution is failed`() = runTest {
+        val result = githubUserUseCase("name")
+        assert(result.isFailure)
     }
 
 }
