2a060f0a3379fda4f5b42b1b7e4a3e42726c34f6
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-06 00:22:20 +0900

    #7 : github data to flow

--- app/src/main/java/com/evergreen/todaycommit/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/evergreen/todaycommit/presentation/main/MainViewModel.kt
@@ -1,25 +1,39 @@
 package com.evergreen.todaycommit.presentation.main
 
 import android.util.Log
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.evergreen.todaycommit.domain.model.GithubUser
 import com.evergreen.todaycommit.domain.usecase.GetGithubUserUseCase
+import com.evergreen.todaycommit.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val githubUserUseCase: GetGithubUserUseCase
-) : ViewModel() {
+) : BaseViewModel() {
 
-    fun fetchUser(){
+    private val _githubUser = MutableStateFlow<GithubUser?>(null)
+    val githubUser: StateFlow<GithubUser?>
+        get() = _githubUser.asStateFlow()
+
+    init {
+        fetchUser()
+    }
+
+    private fun fetchUser() {
         viewModelScope.launch(Dispatchers.IO) {
             githubUserUseCase("EvergreenTree97").onSuccess {
-                Log.d("성공", "fetchUser: ${it.userName}")
+                Log.d("성공", it.userName)
+                _githubUser.emit(it)
             }.onFailure {
-                Log.d("실패", "fetchUser: ${it.localizedMessage} ")
+                Log.d("실패", it.localizedMessage)
+                emitException(it)
             }
         }
     }
