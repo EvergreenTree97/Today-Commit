227e578ec80a1cfd7ed51794160f011412b4e438
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-07 20:42:29 +0900

    #8 : threeten init

--- app/src/main/java/com/evergreen/todaycommit/presentation/main/MainActivity.kt
+++ app/src/main/java/com/evergreen/todaycommit/presentation/main/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.evergreen.todaycommit.presentation.theme.TodayCommitTheme
 import com.google.firebase.messaging.FirebaseMessaging
+import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidThreeTen.init(this);
         val token = FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
             if (!task.isSuccessful) {
                 Log.w("실패", "Fetching FCM registration token failed", task.exception)
