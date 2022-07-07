a93990eff1185ed64f361104fcb2f7d4d700d7e4
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-07 20:43:45 +0900

    #8 : add animation refresh button

--- app/src/main/java/com/evergreen/todaycommit/presentation/main/components/MainComposable.kt
+++ app/src/main/java/com/evergreen/todaycommit/presentation/main/components/MainComposable.kt
@@ -1,20 +1,32 @@
 package com.evergreen.todaycommit.presentation.main.components
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.evergreen.todaycommit.R
 import com.evergreen.todaycommit.presentation.theme.White
 
 @Composable
 internal fun RoundCornerBox(
     modifier: Modifier,
     alignment: Alignment = Alignment.CenterStart,
-    content: @Composable () -> Unit
+    content: @Composable BoxScope.() -> Unit
 ) {
     Box(
         modifier = modifier
@@ -24,4 +36,33 @@ internal fun RoundCornerBox(
     ) {
         content()
     }
+}
+
+@Composable
+fun AnimationRefresh(
+    fetchUser: () -> Unit
+) {
+    val (isRefreshEnabled, setRefreshEnabled) = remember { mutableStateOf(true) }
+    val (isRefreshRotated, setRefreshRotated) = remember { mutableStateOf(false) }
+    val angle: Float by animateFloatAsState(
+        targetValue = if (isRefreshRotated) 1080F else 0F,
+        animationSpec = tween(
+            durationMillis = 2000,
+            easing = FastOutSlowInEasing
+        ),
+        finishedListener = {
+            setRefreshEnabled(true)
+        }
+    )
+    Image(
+        modifier = Modifier
+            .clickable(enabled = isRefreshEnabled) {
+                fetchUser()
+                setRefreshRotated(!isRefreshRotated)
+                setRefreshEnabled(false)
+            }
+            .rotate(angle),
+        painter = painterResource(id = R.drawable.refresh),
+        contentDescription = "refresh"
+    )
 }
