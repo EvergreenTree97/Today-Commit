bcfc505a40fd97367254b4bbbb1f0c6920698ffe
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-13 00:02:10 +0900

    #9 : add StartActivity

--- app/src/main/java/com/evergreen/todaycommit/presentation/onboarding/StartActivity.kt
+++ app/src/main/java/com/evergreen/todaycommit/presentation/onboarding/StartActivity.kt
@@ -1,4 +1,20 @@
 package com.evergreen.todaycommit.presentation.onboarding
 
-class StartActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.evergreen.todaycommit.presentation.onboarding.login.UserNameInputScreen
+import com.evergreen.todaycommit.presentation.theme.TodayCommitTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class StartActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodayCommitTheme {
+                LoginScreen()
+            }
+        }
+    }
 }
