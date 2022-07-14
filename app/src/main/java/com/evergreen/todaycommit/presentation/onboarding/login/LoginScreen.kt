56aea6d23cd297104aae315c8cc8d8dc63ac2e71
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-14 16:42:52 +0900

    #9 : add navigation to UserNameInputScreen and stringresource

--- app/src/main/java/com/evergreen/todaycommit/presentation/onboarding/login/LoginScreen.kt
+++ app/src/main/java/com/evergreen/todaycommit/presentation/onboarding/login/LoginScreen.kt
@@ -1,4 +1,4 @@
-package com.evergreen.todaycommit.presentation.onboarding
+package com.evergreen.todaycommit.presentation.onboarding.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -9,14 +9,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.evergreen.todaycommit.R
@@ -28,11 +26,13 @@ import com.evergreen.todaycommit.presentation.theme.Green300
 @Preview
 @Composable
 private fun LoginPreview() {
-    LoginScreen()
+    LoginScreen{}
 }
 
 @Composable
-internal fun LoginScreen() {
+internal fun LoginScreen(
+    navigateToOnBoarding: () -> Unit
+) {
     Column(
         modifier = Modifier
             .background(Background)
@@ -49,11 +49,11 @@ internal fun LoginScreen() {
                 .padding(start = 20.dp, end = 20.dp)
                 .fillMaxWidth()
                 .height(48.dp),
-            onClick = { /*TODO*/ },
+            onClick = { navigateToOnBoarding() },
             color = Green300
         ) {
             Text(
-                text = "로그인",
+                text = stringResource(id = R.string.login),
             )
         }
         Spacer(modifier = Modifier.height(44.dp))
